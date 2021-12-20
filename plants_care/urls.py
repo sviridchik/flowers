ecd80fffdbc00032e90057b878c60f17bf98deb5
victoria <victoria@wsa-219-72a.icx.local>
2021-12-20 13:30:53 +0300

    some basic  crud

--- plants_care/urls.py
+++ plants_care/urls.py
@@ -7,6 +7,12 @@ from . import views
 # app_name = "plants_care"
 
 urlpatterns = [
-    path(r"fertilizer/", views.FertilizerList.as_view()),
-    path(r"fertilizer/" + pk_reg + "$", views.FertilizerListDetail.as_view(), name="detail"),
+    path(r"fertilizer/", views.FertilizerList.as_view(), name="fertilizer_list"),
+    path(r"fertilizer/" + pk_reg + "$", views.FertilizerListDetail.as_view(), name="fertilizer_detail"),
+    path(r"watering/", views.WateringList.as_view(), name="watering_list"),
+    path(r"watering/" + pk_reg + "$", views.WateringListDetail.as_view(), name="watering_detail"),
+    path(r"solutions/", views.SolutionsList.as_view(), name="solutions_list"),
+    path(r"solutions/" + pk_reg + "$", views.SolutionsListDetail.as_view(), name="solutions_detail"),
+    path(r"regimes/", views.RegimeList.as_view(), name="regimes_list"),
+    path(r"regimes/" + pk_reg + "$", views.RegimeListDetail.as_view(), name="regimes_detail"),
 ]
--- plants_care/views.py
+++ plants_care/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 
-from .models import Fertilizer
+from .models import Fertilizer, Watering
 from .serializers import *
 
 
@@ -14,5 +14,47 @@ class FertilizerList(generics.ListCreateAPIView):
 class FertilizerListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Fertilizer.objects.all()
     serializer_class = FertilizerSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class WateringList(generics.ListCreateAPIView):
+    queryset = Watering.objects.all()
+    serializer_class = WateringSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class WateringListDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Watering.objects.all()
+    serializer_class = WateringSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class SolutionsList(generics.ListCreateAPIView):
+    queryset = Solution.objects.all()
+    serializer_class = SolutionSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class SolutionsListDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Solution.objects.all()
+    serializer_class = SolutionSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class RegimeList(generics.ListCreateAPIView):
+    queryset = Regime.objects.all()
+    serializer_class = RegimeSerializer
+    # TODO: maybe allowANy?
+    # permission_classes = (permissions.IsAuthenticated)
+
+
+class RegimeListDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Regime.objects.all()
+    serializer_class = RegimeSerializer
     # TODO: maybe allowANy?
     # permission_classes = (permissions.IsAuthenticated)
