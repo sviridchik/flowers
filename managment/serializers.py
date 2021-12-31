e9600bd43109bd39adc8126655b84db6b1ae97cf
VictoriaS <viccisviri@gmail.com>
2021-12-31 14:11:13 +0300

    looks like fixed bug with nested

--- managment/serializers.py
+++ managment/serializers.py
@@ -35,8 +35,34 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer(read_only=True)
+    # TODO not sure if it is a good idea
+    profile = ProfileSerializer(read_only=True, many = True)
 
     class Meta:
         model = Rooms
         fields = ("id", "humidity_summer", "humidity_winter", "temp_winter", "temp_summer", "profile")
+
+    def create(self, validated_data):
+        profiles_data = validated_data.pop('profile')
+        room = Rooms.objects.create(**validated_data)
+        for profile_data in profiles_data:
+            profile, created = Ingredient.objects.get_or_create(username=profile_data['username'])
+            room.profile.add(profile)
+        return room
+
+    def update(self, instance, validated_data):
+        profiles_data = validated_data.pop('profile')
+        instance.humidity_summer = validated_data.get('humidity_summer', instance.humidity_summer)
+        instance.last_name = validated_data.get('temp_winter', instance.temp_winter)
+        instance.temp_winter = validated_data.get('humidity_winter', instance.humidity_winter)
+        instance.temp_summer = validated_data.get('temp_summer', instance.temp_summer)
+
+        instance.save()
+
+        for profile_data in profiles_data:
+            profile = profiles.pop(0)
+            profile.username = profile_data.get('username', profile.username)
+            profile.password = profile_data.get('password', profile.password)
+            profile.email = profile_data.get('email', profile.email)
+            profile.save()
+        return instance
