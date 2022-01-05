42b0f965d428cbda4af174bc769ef5407d0d2f1e
victoria <victoria@wsa-219-72a.icx.local>
2022-01-05 12:40:46 +0300

    big secret realead to me

--- managment/serializers.py
+++ managment/serializers.py
@@ -2,35 +2,37 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from managment.models import Rooms, Profile
+from managment.models import Profile, Rooms
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=150, source='user.username')
-    password = serializers.CharField(max_length=128, source='user.password')
-    email = serializers.EmailField(max_length=128, source='user.email')
+    username = serializers.CharField(max_length=150, source="user.username")
+    password = serializers.CharField(max_length=128, source="user.password")
+    email = serializers.EmailField(max_length=128, source="user.email")
 
     class Meta:
         model = Profile
-        fields = ('email', 'level_of_qualification', 'username', 'password')
+        fields = ("email", "level_of_qualification", "username", "password")
 
     def create(self, validated_data):
-        password = make_password(validated_data.get('user')['password'])
+        password = make_password(validated_data.get("user")["password"])
         user, created = User.objects.get_or_create(
-            username=validated_data.get('user')['username'],
-            defaults={'email': validated_data.get('user')['email'], 'password': password})
+            username=validated_data.get("user")["username"],
+            defaults={"email": validated_data.get("user")["email"], "password": password},
+        )
 
-        profile, created = Profile.objects.get_or_create(user=user,
-                                                         defaults={'level_of_qualification': validated_data.get(
-                                                             'level_of_qualification')})
+        profile, created = Profile.objects.get_or_create(
+            user=user, defaults={"level_of_qualification": validated_data.get("level_of_qualification")}
+        )
         return profile
 
     def update(self, instance, validated_data):
         user = validated_data.get("user", {})
-        instance.user.email = user.get('email', instance.user.email)
-        instance.user.username = user.get('username', instance.user.username)
-        instance.user.password = validated_data.get('password', instance.user.password)
-        instance.level_of_qualification = validated_data.get('level_of_qualification', instance.level_of_qualification)
+        instance.user.email = user.get("email", instance.user.email)
+        instance.user.username = user.get("username", instance.user.username)
+        instance.user.password = validated_data.get("password", instance.user.password)
+        instance.level_of_qualification = validated_data.get("level_of_qualification", instance.level_of_qualification)
+        instance.save()
         return instance
 
 
