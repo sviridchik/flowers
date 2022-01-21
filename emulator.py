8f57455f6e22ad3141ae7bbd21c5df174eff15a2
VictoriaS <viccisviri@gmail.com>
2022-01-21 17:02:33 +0300

    done

--- emulator.py
+++ emulator.py
@@ -6,22 +6,25 @@ from asyncio import exceptions
 
 import aiohttp
 
-# TODO for more serios fields
-import faker
-
 from COSTANTS import DISTRIBUTION_MAP
 from plants_base.choices import TypeChoice
 
 logging.basicConfig(level=logging.INFO)
 
-with open("config.json") as f:
-    data_constants_raw = json.load(f)
-data_constants = {}
-for i in range(len(data_constants_raw["VALUE_TO_CHANGE"])):
-    node = data_constants_raw["VALUE_TO_CHANGE"][i]
-    data_constants[node["field"]] = DISTRIBUTION_MAP[node["distribution"]](node["range"][0], node["range"][1])
-    if "function" in node:
-        data_constants[node["field"]] = DISTRIBUTION_MAP[node["function"]](data_constants[node["field"]])
+
+def prep_data(DISTRIBUTION_MAP):
+    with open("config.json") as f:
+        data_constants_raw = json.load(f)
+    data_constants = {}
+    for i in range(len(data_constants_raw["VALUE_TO_CHANGE"])):
+        node = data_constants_raw["VALUE_TO_CHANGE"][i]
+        data_constants[node["field"]] = DISTRIBUTION_MAP[node["distribution"]](node["range"][0], node["range"][1])
+        if "function" in node:
+            data_constants[node["field"]] = DISTRIBUTION_MAP[node["function"]](data_constants[node["field"]])
+    return data_constants, data_constants_raw
+
+
+data_constants, data_constants_raw = prep_data(DISTRIBUTION_MAP)
 
 
 async def choose_the_victim_plant(session):
@@ -41,26 +44,27 @@ async def choose_the_victim_plant(session):
             data={field_to_change: value},
         ) as response:
             response_patch = await response.json()
-
-        status = response.status
+            status = response.status
         if status == 200:
             state = "successfully"
         else:
             state = "unsuccessfully"
-        logging.info(f"Metric that was {state} changed is {field_to_change}, with value {value}")
+        logging.info(
+            f"Metric that was {state} changed is {field_to_change}, with value {value}  reason: {response_patch} "
+        )
         logging.info(response_patch)
     else:
         logging.warning(f"Metric wasn't changed , reason: {response.json}")
-    await asyncio.sleep(1)
+    timeout = random.uniform(1, 5)
+    await asyncio.sleep(timeout)
 
 
 async def main():
-    N = 3
+    n = 3
     async with aiohttp.ClientSession() as session:
-        while N > 0:
-            N -= 1
-            task1 = asyncio.create_task(choose_the_victim_plant(session))
-            await asyncio.gather(task1)
+        tasks = [asyncio.create_task(choose_the_victim_plant(session)) for _ in range(n)]
+        for i in range(n):
+            await asyncio.gather(tasks[i])
 
 
 if __name__ == "__main__":
@@ -68,4 +72,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(asyncio.wait_for(main(), data_constants_raw["DEFAULT_TIMEOUT"]))
     except exceptions.TimeoutError:
-        print("the end")
+        logging.info("the end")
